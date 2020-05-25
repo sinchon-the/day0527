fc6491fd0db2b2e10a69da951246ae40b271bbdc
ssoxxi <53103312+ssoxxi@users.noreply.github.com>
2020-05-25 11:29:26 +0900

    한소희_Person

--- Person.java
+++ Person.java
@@ -1,5 +1,30 @@
 package day0525;
-
+/*
 public class Person {
+	String name;
+	int age;
+}*/
+//보안상 문제 있음
+//default 존재->패키지 타입 -> 접근 불가능하도록 private
 
+//캡슐화 개념 처리
+public class Person {
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
