9c1183e524cc2ff18a56767812c2b2e3965b3245
ssoxxi <53103312+ssoxxi@users.noreply.github.com>
2020-05-26 17:11:01 +0900

    java_sohee

--- Student.java
+++ Student.java
@@ -1,28 +1,35 @@
-package Student;
 
-public class Student {
-		private String name;//private로 외부에서 접근 못하게 하고 클래스 내부에서만 사용할 수 있습니다.
-		private int age;//private로 외부에서 접근 못하게 하고 클래스 내부에서만 사용할 수 있습니다.
-		
-		//public은 어디서든 접근이 가능합니다
-		//getName() 멤버필드 name의 값을 얻어오는 메서드
-		public String getName() {
-			return name;
-		}
-		//setName() 셋팅 해주는 
-		public void setName(String name) {
-			this.name = name;
-		}
-		//getAge() class 멤버인 age의 값을 얻어오는 메서드
-		public int getAge() {
-			return age;
-		}
-		//setName() 셋팅 해주는 
-		public void setAge(int age) {
-			this.age = age;
-		}
-		
-		
+/*class Student {
+	String name;
+	int age;
+}*/
 
+/*
+ *
+ * 외부에서 직접적으로 접근이 가능하므로 멤버필드에 접근제한자 private를 붙여준다.
+ * private의 경우 외부에서 접근할 수 없으므로 캡슐화하기위해서  get,set메서드를 public modifier로 생해준다.
+ * class에서는 public class가 하나만 존재한다.->public은 어디서든 접근이 가능하다
+ * get()메소드는 값을 얻어오는 메소드, set()메소드는 값을 설정하는 메소드
+ *
+ */
 
+public class Student{
+	private String name;//외부 클래스 접근 x
+	private int age;
+	
+	public String getName() { //return형 타입 주의!
+		//private 멤버필드에 접근하여 값을 얻어오는 메소드
+		return name;//return 으로 name 값 반환
+	}
+	public void setName(String name) {//return형 타입 주의!
+		//private 멤버필드에 접근하여 값을 설정하는 메소드
+		this.name = name; //this키워드로 바깥에 있는 name에 접근
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
