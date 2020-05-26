7eaccff278e64de024af047cdc30b898844877b2
sillon26 <65876939+sillon26@users.noreply.github.com>
2020-05-26 17:31:31 +0900

    Add files via upload

---
+++ Student_kimbumjin.java
@@ -0,0 +1,26 @@
+package map;
+
+public class Student {
+//	String name;//접근제한자를 default타입으로 설정해서 외부에서 다이렉트로 접근이 가능하다.
+//	int age;//접근제한자를 private로 설정하여 클래스 내에서만 접근을 할 수 있도록 해준다.
+	//private의 경우 외부에서 접근할 수 없으므로 캡슐화하기위해서  get,set메서드를 public modifier로 생성해준다.
+	//
+	private String name;
+	private int age;
+	
+	
+	public String getName() {
+		return name;
+	}//getName 은 위에 잇는 name 멤버 필드 값을 얻어오는 메서드이다.
+	public void setName(String name) {
+		this.name = name;
+	}//name 값을 설정함
+	public int getAge() {
+		return age;
+	}//getAge 는 위에 있는 class 필드의 age값을 얻어오는 메서드이다.
+	public void setAge(int age) {
+		this.age = age;
+	}//age 값을 설정함
+
+
+}
