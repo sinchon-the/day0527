35bb8cb372c430ae7012885620832d5ea64c16df
cooljsi <65692912+cooljsi@users.noreply.github.com>
2020-05-29 11:47:31 +0900

    Add files via upload

---
+++ Person_정선일.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	
+	String getName() {
+		return name;
+	}
+	void setName(String name) {
+		this.name = name;
+	}
+	int getAge() {
+		return age;
+	}
+	void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
